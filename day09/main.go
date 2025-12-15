ebf6e042b51b94da78de6c56c6827b9b73fcfb3e
Nick Sidawy <nick.sidawy@gmail.com>
2025-12-15 10:44:29 -0500

    Day09 yeesh

--- day09/main.go
+++ day09/main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"sort"
 	"strconv"
 	"strings"
 )
@@ -33,7 +32,6 @@ func main() {
 
 	fmt.Println(findMaxArea1(redTiles))
 	fmt.Println(findMaxArea2(redTiles))
-	//getTilesMap(redTiles)
 	fmt.Println("done")
 }
 
@@ -51,71 +49,74 @@ func findMaxArea1(redTiles []Point) int {
 }
 
 func findMaxArea2(redTiles []Point) int {
-	maxArea := 2000000000
-	emptyTiles := getTilesMap(redTiles)
+	maxArea := 0
+	borderTiles := getBorderTiles(redTiles)
 	for i := 0; i < len(redTiles); i++ {
+		fmt.Println(i, maxArea)
 		for j := i + 1; j < len(redTiles); j++ {
 			area := getArea(redTiles[i], redTiles[j])
-			if area > maxArea && isValidArea(redTiles[i], redTiles[j], emptyTiles) {
-				maxArea = area
-				fmt.Println(redTiles[i], redTiles[j], area)
+			if area > maxArea {
+				if isValidArea(redTiles[i], redTiles[j], redTiles, borderTiles) {
+					maxArea = area
+					fmt.Println("Valid area found:", redTiles[i], redTiles[j], area)
+				}
 			}
 		}
 	}
 	return maxArea
 }
 
-func findMaxArea3(redTiles []Point) int {
-	emptyTiles := getTilesMap(redTiles)
-	pairs := make([]Pair, 0, len(redTiles)*(len(redTiles)-1)/2)
-	for i := 0; i < len(redTiles); i++ {
-		for j := i + 1; j < len(redTiles); j++ {
-			pairs = append(pairs, Pair{p1: redTiles[i], p2: redTiles[j], area: getArea(redTiles[i], redTiles[j])})
-		}
-	}
-	sort.Slice(pairs, func(i, j int) bool {
-		return pairs[i].area > pairs[j].area
-	})
-	fmt.Println(len(pairs))
-	for i, pair := range pairs {
-		if i%1000 == 0 {
-			fmt.Println(i, pair.area)
-		}
-		if isValidArea(pair.p1, pair.p2, emptyTiles) {
-			return pair.area
-		}
-	}
-	return 0
-}
-
-func isValidArea(p1 Point, p2 Point, emptyTiles [][]bool) bool {
+func isValidArea(p1 Point, p2 Point, redTiles []Point, borderTiles map[Point]bool) bool {
 	lowerX := min(p1.x, p2.x)
 	higherX := max(p1.x, p2.x)
 	lowerY := min(p1.y, p2.y)
 	higherY := max(p1.y, p2.y)
-	for x := lowerX; x <= higherX; x++ {
-		for y := lowerY; y <= higherY; y++ {
-			if emptyTiles[x][y] {
-				return false
-			}
-		}
+	if !isTileInBorder(Point{x: p1.x, y: p2.y}, borderTiles) || !isTileInBorder(Point{x: p2.x, y: p1.y}, borderTiles) {
+		return false
 	}
-	return true
-}
+	for i, _ := range redTiles {
+		point1 := redTiles[i]
+		point2 := redTiles[(i+1)%len(redTiles)]
 
-func isValidArea2(p1 Point, p2 Point, emptyTiles map[Point]bool) bool {
-	lowerX := min(p1.x, p2.x)
-	higherX := max(p1.x, p2.x)
-	lowerY := min(p1.y, p2.y)
-	higherY := max(p1.y, p2.y)
+		if (p1 == point1 || p1 == point2) && (p2 == point1 || p2 == point2) {
+			continue
+		}
 
-	for e := range emptyTiles {
-		if e.x >= lowerX && e.x <= higherX && e.y >= lowerY && e.y <= higherY {
+		// Calculate bounding box for the segment (point1, point2)
+		segLowerX := min(point1.x, point2.x)
+		segHigherX := max(point1.x, point2.x)
+		segLowerY := min(point1.y, point2.y)
+		segHigherY := max(point1.y, point2.y)
+
+		// Check for intersection with bounding box defined by lowerX, higherX, lowerY, higherY
+		if segHigherX <= lowerX || segLowerX >= higherX || segHigherY <= lowerY || segLowerY >= higherY {
+			continue
+		} else {
 			return false
 		}
 	}
-
 	return true
+}
+
+func isTileInBorder(p Point, borderTiles map[Point]bool) bool {
+	current := p
+	count := 0
+	if borderTiles[p] {
+		return true
+	}
+	prevBorder := false
+	for current.x >= 0 {
+		if borderTiles[current] {
+			if !prevBorder {
+				count++
+			}
+			prevBorder = true
+		} else {
+			prevBorder = false
+		}
+		current.x = current.x - 1
+	}
+	return count%2 == 1
 }
 
 func getBorderTiles(redTiles []Point) map[Point]bool {
@@ -147,85 +148,6 @@ func getBorderTiles(redTiles []Point) map[Point]bool {
 	connectTiles(redTiles[0], redTiles[len(redTiles)-1])
 
 	return borderTiles
-}
-
-func getTilesMap(redTiles []Point) [][]bool {
-	minX := 1000000000
-	maxX := 0
-	minY := 1000000000
-	maxY := 0
-	for _, tile := range redTiles {
-		if tile.x > maxX {
-			maxX = tile.x
-		}
-		if tile.x < minX {
-			minX = tile.x
-		}
-		if tile.y > maxY {
-			maxY = tile.y
-		}
-		if tile.y < minY {
-			minY = tile.y
-		}
-	}
-	maxY++
-	maxX++
-	minX--
-	minY--
-
-	borderTiles := getBorderTiles(redTiles)
-
-	emptyTiles := make([][]bool, maxX+1)
-	for i, _ := range emptyTiles {
-		emptyTiles[i] = make([]bool, maxY+1)
-	}
-	queue := make([]Point, maxX*maxY/10)
-	current := 0
-	end := 0
-	addToQueue := func(p Point) {
-		if p.y < maxY {
-			if !borderTiles[Point{x: p.x, y: p.y + 1}] && !emptyTiles[p.x][p.y+1] {
-				queue[end] = Point{x: p.x, y: p.y + 1}
-				end++
-				emptyTiles[p.x][p.y+1] = true
-			}
-		}
-		if p.y > minY {
-			if !borderTiles[Point{x: p.x, y: p.y - 1}] && !emptyTiles[p.x][p.y-1] {
-				queue[end] = Point{x: p.x, y: p.y - 1}
-				end++
-				emptyTiles[p.x][p.y-1] = true
-			}
-		}
-		if p.x < maxX {
-			if !borderTiles[Point{x: p.x + 1, y: p.y}] && !emptyTiles[p.x+1][p.y] {
-				queue[end] = Point{x: p.x + 1, y: p.y}
-				end++
-				emptyTiles[p.x+1][p.y] = true
-			}
-		}
-		if p.x > minX {
-			if !borderTiles[Point{x: p.x - 1, y: p.y}] && !emptyTiles[p.x-1][p.y] {
-				queue[end] = Point{x: p.x - 1, y: p.y}
-				end++
-				emptyTiles[p.x-1][p.y] = true
-			}
-		}
-	}
-	emptyTiles[minX][minY] = true
-	addToQueue(Point{x: minX, y: minY})
-	end++
-	for current < end {
-		p := queue[current]
-		current++
-		addToQueue(p)
-		if current%1000000 == 0 {
-			fmt.Println(current, end, end-current)
-		}
-	}
-
-	fmt.Println("Empty Tiles", len(emptyTiles))
-	return emptyTiles
 }
 
 func getArea(p1 Point, p2 Point) int {
